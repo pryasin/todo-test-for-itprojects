8a7628e93fc73ca2de935ad447d0d4f690eb9839
pryasin <komarekk33@gmail.com>
2020-04-12 12:28:29 +0700

    feat: added Header and Button components

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="header">
+    <h1 class="page-h1">{{ pageTitle }}</h1>
+    <Button variant="secondary" title="Добавить" />
+  </header>
+</template>
+
+<script>
+import Button from './_common/Button.vue';
+
+export default {
+  props: ['pageTitle', 'withButton'],
+  components: {
+    Button
+  }
+};
+</script>
+
+<style>
+.header {
+  margin-top: 24px;
+  margin-bottom: 48px;
+  display: flex;
+}
+
+.page-h1 {
+  margin: 0;
+  margin-right: 24px;
+}
+</style>
---
+++ src/components/_common/Button.vue
@@ -0,0 +1,61 @@
+<template>
+  <button :class="['btn', variant]" type="submit">{{ title }}</button>
+</template>
+
+<script>
+export default {
+  props: ['title', 'variant']
+};
+</script>
+
+<style scoped>
+.btn {
+  box-sizing: border-box;
+  border-radius: 6px;
+  padding: 10px 30px;
+  font-weight: 600;
+  font-size: 14px;
+  appearance: none;
+  transition: all 0.3s;
+}
+
+.primary {
+  background-color: #355eed;
+  border: 2px solid #355eed;
+  color: #fff;
+}
+
+.primary:hover {
+  background-color: #577bf6;
+  border: 2px solid #577bf6;
+}
+
+.secondary {
+  color: #355eed;
+  background-color: #fff;
+  border: 2px solid #355eed;
+}
+
+.secondary:hover {
+  color: #577bf6;
+  background-color: #fff;
+  border: 2px solid #577bf6;
+}
+
+.secondary:hover,
+.primary:hover,
+.secondary:active,
+.primary:active {
+  transition: none;
+}
+
+.secondary:active,
+.primary:active {
+  border: 2px solid #2b4ec9;
+  background-color: #2b4ec9;
+}
+
+.secondary:active {
+  color: #fff;
+}
+</style>
